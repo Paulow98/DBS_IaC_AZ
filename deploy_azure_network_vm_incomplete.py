bbf6d8933bcd9db61aa7d49972e1ccfc03478e96
JamesM53 <jamesmoore@live.ie>
2024-11-15 09:49:38 +0000

    Update deploy_azure_network_vm_incomplete.py
    Added line create subnets for each component

--- deploy_azure_network_vm_incomplete.py
+++ deploy_azure_network_vm_incomplete.py
@@ -21,4 +21,12 @@ vnet_name = "TestVNet"
 network_client = NetworkManagementClient(credentials, subscription_id)
 network_client.virtual_networks.begin_create_or_update(rg_name, vnet_name, {}).result()
 
+# Create Subnets for each component
+for subnet_name, prefix in zip(subnet_names, subnet_prefixes):
+    subnet_params = {"address_prefix": prefix}
+    network_client.subnets.begin_create_or_update(rg_name, vnet_name, subnet_name, subnet_params).result()
+
+
+
+
 # No subnet, NIC, or VM configurations provided
